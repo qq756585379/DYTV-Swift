49e883c2d4901830484307a29e3fd49c6ca85896
杨俊 <756585379@qq.com>
2017-09-03 16:19:58 +0800

    恩

---
+++ DYTV/DYTV/MainUI/Home/HomeVC.swift
@@ -0,0 +1,37 @@
+//
+//  HomeVC.swift
+//  DYTV
+//
+//  Created by 杨俊 on 2017/8/30.
+//  Copyright © 2017年 Lenovo-Apple. All rights reserved.
+//
+
+import UIKit
+
+private let kTitleViewH : CGFloat = 40
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DYTV/DYTV/MainUI/Main/DouYuTBC.swift
+++ DYTV/DYTV/MainUI/Main/DouYuTBC.swift
@@ -12,20 +12,29 @@ class DouYuTBC: BaseTBC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+		addChildViewControllers()
+        
+        let backView = UIView(frame: CGRect(x: 0, y: 0, width: kScreenW, height: 49))
+        backView.backgroundColor = UIColor.white
+        tabBar.insertSubview(backView, at: 0)
+        tabBar.isOpaque = true
     }
 
     fileprivate func addChildViewControllers() {
-        
+        setupOneChildViewController(title: "首页", image: "btn_home_normal", selectedImage: "btn_home_selected", controller: HomeVC())
+        setupOneChildViewController(title: "直播", image: "menu_youxi_nor", selectedImage: "menu_youxi_sel", controller: LiveVC())
+        setupOneChildViewController(title: "关注", image: "menu_yule_nor", selectedImage: "menu_yule_sel", controller: FollowVC())
+        setupOneChildViewController(title: "我的", image: "menu_goddess_nor", selectedImage: "menu_goddess_sel", controller: ProfileVC())
     }
     
-//    fileprivate func setupOneChildViewController(title: String, image: String, selectedImage: String, controller: UIViewController) {
-//        controller.tabBarItem.title = title
-//        controller.title = title
-//        controller.view.backgroundColor = UIColor.white
-//        controller.tabBarItem.image = UIImage(named: image)
-//        controller.tabBarItem.selectedImage = UIImage(named: selectedImage)
-//        let naviController = NavigationController.init(rootViewController: controller)
-//        addChildViewController(naviController)
-//    }
+    fileprivate func setupOneChildViewController(title: String, image: String, selectedImage: String, controller: UIViewController) {
+        controller.tabBarItem.title = title
+        controller.title = title
+        controller.view.backgroundColor = UIColor.white
+        controller.tabBarItem.image = UIImage(named: image)
+        controller.tabBarItem.selectedImage = UIImage(named: selectedImage)
+        let naviController = BaseNC.init(rootViewController: controller)
+        addChildViewController(naviController)
+    }
+    
 }
